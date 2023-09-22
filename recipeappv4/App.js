52381d0657a60baba4d19cf8e2795fc06278ba5b
YahefuYusufu <parguzatt@gmail.com>
2023-09-23 00:41:33 +0200

    ready to hick,weather app

---
+++ recipeappv4/App.js
@@ -0,0 +1,6 @@
+import { StatusBar } from "expo-status-bar"
+import Navigation from "./src/navigation/navigation"
+
+export default function App() {
+	return <Navigation />
+}
---
+++ recipeappv4/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React, { useCallback, useState, useEffect } from "react"
+import {
+	StyleSheet,
+	Text,
+	View,
+	TouchableOpacity,
+	Image,
+	TextInput,
+	ScrollView,
+} from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const HomeScreen = () => {
+	return (
+		<View className="flex-1 justify-center items-center bg-blue-400">
+			<Text>HomeScreen</Text>
+		</View>
+	)
+}
+
+export default HomeScreen
