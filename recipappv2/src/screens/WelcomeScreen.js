0b179c1dece7de965a7f278ec7766e7e3f95dbbc
YahefuYusufu <parguzatt@gmail.com>
2023-09-22 22:20:13 +0200

    got error of about tailwindcss

--- recipappv2/src/screens/WelcomeScreen.js
+++ recipappv2/src/screens/WelcomeScreen.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { Text, View } from "react-native"
 
 const WelcomeScreen = () => {
 	return (
 		<View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
 			<StatusBar style="light" />
 			<Text className="text-blue-500">WelcomeScreen</Text>
+			<Text></Text>
 		</View>
 	)
 }
